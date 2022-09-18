2985c973434de5415b385e232f59a90257a1dd71
python3boto3 <python3.boto3@gmail.com>
2022-09-17 17:18:38 -0700

    updated with where clauses, drop

--- py-to-mariadb.py
+++ py-to-mariadb.py
@@ -2,6 +2,7 @@
 
 import mysql.connector as mariadb
 
+
 # create mysql connection and define cursor
 
 mariadb_connection = mariadb.connect(
@@ -13,6 +14,7 @@ mariadb_connection = mariadb.connect(
 
 create_cursor = mariadb_connection.cursor()
 
+
 # create database
 
 create_cursor.execute("CREATE DATABASE test_db")
@@ -20,11 +22,13 @@ create_cursor.execute("CREATE DATABASE test_db")
 for x in create_cursor:
     print(x)
 
+
 # create table
 
 create_cursor.execute(
     "CREATE_TABLE python_created_table (COLUMN1 VARCHAR(2), COLUMN2 int)"
     )
+
 
 # show tables
 
@@ -32,3 +36,58 @@ create_cursor.execute("SHOW TABLES")
 
 for x in create_cursor:
     print(x)
+
+# insert into mysql table
+
+sql_statement = "INSERT INTO python_created_table \
+(COLUMN1, COLUMN2) VALUES (%s, %s)"
+items_to_insert = ('hi', 4)
+
+create_cursor.execute(sql_statement, items_to_insert)
+
+mariadb_connection.commit()
+
+
+# select items from mysql
+
+sql_statement = 'SELECT * from python_creation_table'
+
+create_cursor.execute(sql_statement)
+
+myresult = create_cursor.fetchall()  # fetchnone();
+
+print(myresult)
+
+
+# use of where clause
+
+sql_statement = 'SELECT * from python_creation_table where COLUMN2 = 3'
+
+myresult = create_cursor.fetchall()  # fetchnone();
+
+print(myresult)
+
+
+# more complicated
+
+sql_statement = 'SELECT sum(COLUMN2) as sum_col_2 from python_creation_table \
+where COLUMN2 < 3 group by COLUMN2 order by 1'
+
+create_cursor.execute(sql_statement)
+
+myresult = create_cursor.fetchall()  # fetchnone();
+
+print(myresult)
+
+# drop
+
+create_cursor.execute("DROP TABLE python_created table")
+
+create_cursor.execute("SHOW tables")
+
+for x in create_cursor:
+    print(x)
+
+# close connection
+
+mariadb_connection.close()
