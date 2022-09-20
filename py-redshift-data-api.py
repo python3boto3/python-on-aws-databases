b63ea71db2ac5e4e6cd531f929f154b4a9fd859c
python3boto3 <python3.boto3@gmail.com>
2022-09-20 07:16:29 -0700

    updated to pep-8 standard

--- py-redshift-data-api.py
+++ py-redshift-data-api.py
@@ -13,9 +13,9 @@ import psycopg2
 rsd = boto3.client('redshift-data', region_name='us-east-1')
 
 resp = rsd.execute_statement(
-Database="dev",
-ClusterIdentifier='redshift-cluster-1',
-DbUser="redshift_data_api_user",
-Sql="DELETE FROM dev.",
-WithEvent=True
+    Database="dev",
+    ClusterIdentifier='redshift-cluster-1',
+    DbUser="redshift_data_api_user",
+    Sql="DELETE FROM dev.",
+    WithEvent=True
 )
