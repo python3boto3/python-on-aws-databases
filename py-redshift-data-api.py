542e5ae460e83c5a0838076c604fcfccf2bc8abd
python3boto3 <python3.boto3@gmail.com>
2022-09-20 07:13:32 -0700

    updated api

--- py-redshift-data-api.py
+++ py-redshift-data-api.py
@@ -1,1 +1,21 @@
 import boto3
+import psycopg2
+
+# https://aws.amazon.com/blogs/big-data/
+
+# See the section on :
+# using-the-amazon-redshift-data-api-
+# to-interact-with-amazon-redshift-clusters/
+
+# session = boto3.session.Session(profile_name="red")
+# rsd = session.client('redshift-data', region_name='us-east-1')
+
+rsd = boto3.client('redshift-data', region_name='us-east-1')
+
+resp = rsd.execute_statement(
+Database="dev",
+ClusterIdentifier='redshift-cluster-1',
+DbUser="redshift_data_api_user",
+Sql="DELETE FROM dev.",
+WithEvent=True
+)
