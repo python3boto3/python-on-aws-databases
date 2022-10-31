de6ab8eece6bf856c42def40f4508643d8481c9b
python3boto3 <python3.boto3@gmail.com>
2022-10-31 13:28:58 -0700

    s3 addition

---
+++ py-to-s3.py
@@ -0,0 +1,13 @@
+import boto3
+
+some_binary_data = b'Here we have some data'
+more_binary_data = b'Here we have some more data'
+
+# Method 1: Object.put()
+s3 = boto3.resource('s3')
+object = s3.Object('my_bucket_name', 'my/key/including/filename.txt')
+object.put(Body=some_binary_data)
+
+# Method 2: Client.put_object()
+client = boto3.client('s3')
+client.put_object(Body=more_binary_data, Bucket='my_bucket_name', Key='my/key/including/anotherfilename.txt')
