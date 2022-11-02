e9008e3e6ba28298c764e5fbbb95b4671ad244be
python3boto3 <python3.boto3@gmail.com>
2022-11-01 22:43:36 -0700

    redshift psycopg2

--- py-redshift-data-api.py
+++ py-redshift-data-api.py
@@ -1,12 +1,14 @@
-from tarfile import PAX_NAME_FIELDS
-from typing import Any
+import json
 import boto3
 import psycopg2
-import pandas
-import pandas_redshift as pr
 import pandas as pd
-import logging
 
+
+# Local test development only. 
+# To allow redshift accessibility outside the vpc: add an elastic address (EIP)
+# # On the cluster console, add Actions: "Modify publicly accessible setting".
+# Enable .. and reference the EIP
+# Wait 10 minutes to allow the cluster to modify. 
 
 # https://aws.amazon.com/blogs/big-data/
 
@@ -14,53 +16,46 @@ import logging
 # using-the-amazon-redshift-data-api-
 # to-interact-with-amazon-redshift-clusters/
 
+# local session variable only
 session = boto3.session.Session(profile_name="red")
 # 
-rsd = session.client('redshift-data', region_name='us-east-1')
-
-#rsd = boto3.client('redshift-data', region_name='us-east-1')
-
-resp = rsd.execute_statement(
-    Database="dev",
-    ClusterIdentifier='redshift-cluster-1',
-    DbUser="awsuser",
-    Sql="SELECT * FROM public.cqpocsredshiftdemo",
-    WithEvent=True
-)
-
-# Credentials can be set using different methodologies. For this test,
-# I ran from my local machine which I used cli command "aws configure"
-# to set my Access key and secret access key
+# Credentials can be set using different methodologies, secrets, credential tokens, etc. For this test,
+# I ran from my local machine which I used cli command "aws configure" using RBAC to access via IAM role policies, 
+# combined with get_cluster_credentials hich returns an RBAC generated token.
  
-client2 = session.client(service_name='redshift',
-                      region_name='us-east-1')
+client = session.client(service_name='redshift',
+                      region_name='us-east-1')                                                      # # # # # # # # # # # # # 
 #
-#Using boto3 to get the Database password instead of hardcoding it in the code
+# Therefore using boto3 to get the Database password token instead of hardcoding it in the code. Secrets mgr also preferable. 
+# Currently no access to that...
 #
-cluster_creds = client2.get_cluster_credentials(
-                         DbUser='awsuser',
-                         DbName='dev',
-                         ClusterIdentifier='redshift-cluster-1',
+cluster_creds = client.get_cluster_credentials(
+                         DbUser='awsuser',                                                          # # # # # # # # # # # # # 
+                         DbName='dev',                                                              # # # # # # # # # # # # # 
+                         ClusterIdentifier='redshift-cluster-1',                                    # # # # # # # # # # # # # 
                          AutoCreate=False)
- 
+
 try:
     # Database connection below that uses the DbPassword that boto3 returned
     conn = psycopg2.connect(        
-                host = 'redshift-cluster-1.cjqx1l9dqfg2.us-east-1.redshift.amazonaws.com',
+                host = 'redshift-cluster-1.cjqx1l9dqfg2.us-east-1.redshift.amazonaws.com',          # # # # # # # # # # # # # 
                 port = '5439',
                 user = cluster_creds['DbUser'],
                 password = cluster_creds['DbPassword'],
                 database = 'dev'
-                )
+                )                
+
     # Verifies that the connection worked
     cursor = conn.cursor()
-    cursor.execute("SELECT VERSION()")
-    results = cursor.fetchone()
-    ver = results[0]
-    if (ver is None):
-        print("Could not find version")
+    cursor.execute("select catname FROM dev.public.category")                                       # # # # # # # # # # # # # 
+    results = cursor.fetchall()
+    sqlResult = results
+    if (results is None):    
+        print("Could not find results")
     else:
-        print("The version is " + ver)
+        # returns the collection of all rows. 
+        print(sqlResult)
+
  
 except:
    print('Failed to open database connection.')
