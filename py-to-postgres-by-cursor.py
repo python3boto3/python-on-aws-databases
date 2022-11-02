3ff9f09fefffc8e4214be0667ce75829def8eb04
python3boto3 <python3.boto3@gmail.com>
2022-11-01 22:51:41 -0700

    layers info

--- py-to-postgres-by-cursor.py
+++ py-to-postgres-by-cursor.py
@@ -29,3 +29,39 @@ cur.close()
 
 # close the connection
 con.close()
+
+'''
+
+(In-VPC):
+
+Create  Lambda-layer. On Cloud9 only
+
+https://towardsdatascience.com/python-packages-in-aws-lambda-made-easy-8fbc78520e30
+
+
+mkdir folder
+cd folder
+virtualenv v-env
+source ./v-env/bin/activate             
+pip install psycopg2-binary      <-- The official library
+deactivate
+
+
+mkdir python
+cd python
+cp -r ../v-env/lib64/python3.7/site-packages/* .
+cd ..
+zip -r psycopg2_layer.zip python
+aws lambda publish-layer-version --layer-name psycopg2 --zip-file fileb://psycopg2_layer.zip --compatible-runtimes python3.7
+
+
+
+Add 30 seconds, 1024MB memory 
+
+and these permissions to the lambda: 
+
+AWSLambdaBasicExecutionRole
+AmazonRedshiftFullAccess	
+AmazonRedshiftAllCommandsFullAccess
+AmazonRedshiftDataFullAccess
+'''
