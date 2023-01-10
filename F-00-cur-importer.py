aae3e8dccc1d30e3e9c0899f57fc6ab912b7e81a
JDanson <111497266+python3boto3@users.noreply.github.com>
2023-01-10 13:45:49 -0800

    Update F-00-cur-importer.py

--- F-00-cur-importer.py
+++ F-00-cur-importer.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+
 import gzip
 import boto3, io, time, os
 from pkg_resources import cleanup_resources
